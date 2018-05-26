ee194540fcef160fcc65ef8bede161bd47b11383
Chris Borchert <cborchertrun@gmail.com>
2018-05-26 18:53:47 +0200

    add visualize and netlify deploy

--- src/App.js
+++ src/App.js
@@ -7,12 +7,33 @@ class App extends Component {
   numSets = 2;
 
   state = {
-    pool: this.createDominos(this.numSets).sort(function() {
+    pool: this.createDominos(this.numSets).sort(() => {
       return 0.5 - Math.random();
     }),
     hand: [],
-    discard: []
+    discard: [],
+    featured: []
   };
+
+  featureDomino(i) {
+    this.setState({
+      hand: this.state.hand.filter(domino => domino.id !== i),
+      pool: this.state.pool.filter(domino => domino.id !== i),
+      featured: [
+        ...this.state.pool.filter(domino => domino.id === i),
+        ...this.state.hand.filter(domino => domino.id === i),
+        ...this.state.discard.filter(domino => domino.id === i)
+      ],
+      discard: [...this.state.discard, ...this.state.featured]
+    });
+  }
+
+  unfeatureDomino() {
+    this.setState({
+      featured: [],
+      discard: [...this.state.discard, ...this.state.featured]
+    });
+  }
 
   moveFromHandToDiscard(i) {
     this.setState({
@@ -25,7 +46,7 @@ class App extends Component {
   }
 
   moveFromPoolToHand(i) {
-    if (this.state.hand.length >= this.maxHandSize) {
+    if (this.state.hand.length >= this.maxHandSize && this.maxHandSize > 0) {
       return;
     }
     this.setState({
@@ -34,6 +55,31 @@ class App extends Component {
         ...this.state.hand,
         ...this.state.pool.filter(domino => domino.id === i)
       ]
+    });
+  }
+
+  clearDiscard() {
+    this.setState({
+      pool: [...this.state.pool, ...this.state.discard].sort(() => {
+        return 0.5 - Math.random();
+      }),
+      discard: []
+    });
+  }
+
+  resetDominos() {
+    this.setState({
+      pool: [
+        ...this.state.pool,
+        ...this.state.discard,
+        ...this.state.hand,
+        ...this.state.featured
+      ].sort(() => {
+        return 0.5 - Math.random();
+      }),
+      hand: [],
+      discard: [],
+      featured: []
     });
   }
 
@@ -53,44 +99,101 @@ class App extends Component {
   }
 
   render() {
-    const { pool, hand, discard } = this.state;
+    const { pool, hand, discard, featured } = this.state;
+    const featuredClasses =
+      featured.length > 0 ? "Featured Featured--show" : "Featured";
     console.log(hand);
     return (
       <div className="App">
+        <div
+          className={featuredClasses}
+          onClick={() => {
+            this.unfeatureDomino();
+          }}
+        >
+          {featured.map((domino, i) => (
+            <Domino
+              key={i}
+              first={domino.first}
+              second={domino.second}
+              flipped={true}
+              size="large"
+              onClick={() => {
+                this.unfeatureDomino(domino.id);
+              }}
+            />
+          ))}
+        </div>
         <div className="Hand">
           <h1>Hand</h1>
           <div>
             {hand.map((domino, i) => (
-              <Domino
-                key={i}
-                first={domino.first}
-                second={domino.second}
-                flipped={true}
-                onClick={() => {
-                  this.moveFromHandToDiscard(domino.id);
-                }}
-              />
+              <div className="domino-wrapper">
+                <div
+                  className="button-icon button-icon--domino-show"
+                  onClick={() => {
+                    this.featureDomino(domino.id);
+                  }}
+                >
+                  👁️‍🗨️
+                </div>
+                <Domino
+                  key={i}
+                  first={domino.first}
+                  second={domino.second}
+                  flipped={true}
+                  onClick={() => {
+                    this.moveFromHandToDiscard(domino.id);
+                  }}
+                />
+              </div>
             ))}
           </div>
         </div>
         <div className="Pool">
-          <h1>Pool</h1>
+          <h1>
+            Pool{" "}
+            <span
+              className="button-icon"
+              onClick={this.resetDominos.bind(this)}
+            >
+              🔄
+            </span>
+          </h1>
           <div>
             {pool.map((domino, i) => (
-              <Domino
-                key={i}
-                first={domino.first}
-                second={domino.second}
-                flipped={false}
-                onClick={() => {
-                  this.moveFromPoolToHand(domino.id);
-                }}
-              />
+              <div className="domino-wrapper">
+                <div
+                  className="button-icon button-icon--domino-show"
+                  onClick={() => {
+                    this.featureDomino(domino.id);
+                  }}
+                >
+                  👁️‍🗨️
+                </div>
+                <Domino
+                  key={i}
+                  first={domino.first}
+                  second={domino.second}
+                  flipped={false}
+                  onClick={() => {
+                    this.moveFromPoolToHand(domino.id);
+                  }}
+                />
+              </div>
             ))}
           </div>
         </div>
         <div className="Discard">
-          <h1>Discard</h1>
+          <h1>
+            Discard{" "}
+            <span
+              className="button-icon"
+              onClick={this.clearDiscard.bind(this)}
+            >
+              💣
+            </span>
+          </h1>
           <div>
             {discard.map((domino, i) => (
               <Domino
--- src/components/domino/index.js
+++ src/components/domino/index.js
@@ -9,8 +9,11 @@ const createDots = num => {
   return Array.apply(null, { length: num }).map((a, i) => <Dot key={i} />);
 };
 
-export default ({ first, second, flipped, onClick }) => {
-  const dominoClasses = flipped ? "domino" : "domino domino--hidden";
+export default ({ first, second, flipped, onClick, size }) => {
+  let dominoClasses = flipped ? "domino" : "domino domino--hidden";
+  if (size === "large") {
+    dominoClasses += " domino--large";
+  }
   return (
     <div className={dominoClasses} onClick={onClick}>
       <div className="domino__side">{first}</div>
